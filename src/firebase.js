de072fc6dc16f224fa09e33d301b18cdf41cb621
Vikrant V Joliya <60104131+vikrantvjoliya@users.noreply.github.com>
2021-06-12 09:41:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByM2ekO3goHg-rB2kCPj5etTpSmWqW1iY",
-  authDomain: "klumedia.firebaseapp.com",
-  projectId: "klumedia",
-  storageBucket: "klumedia.appspot.com",
-  messagingSenderId: "1025542311410",
-  appId: "1:1025542311410:web:99272ba118024c1640ef6b",
-  measurementId: "G-SXL1EF5DHP"
+  //YOUR CONFIG
 };
 
   
